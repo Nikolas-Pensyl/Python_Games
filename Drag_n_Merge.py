234a0c28514f14026f02ba82dca918988caa1ec9
Nikolas-Pensyl <45053327+Nikolas-Pensyl@users.noreply.github.com>
2020-07-29 09:18:28 -0400

    big changes

--- Drag_n_Merge.py
+++ Drag_n_Merge.py
@@ -8,12 +8,15 @@ pygame.init()
 
 size = screen_width, screen_height = 800, 1000 #size of board
 
-screen = pygame.display.est_mode(size)
+screen = pygame.display.set_mode(size)
 
 font = pygame.font.SysFont("comicsansms", 45)
+tiles = [[None for x in range(6)] for y in range(8)]
 
 prev_time = 0
+time_left = 1
 mouseDown = False
+game_over = False
 
 cyan = 0, 50, 255
 green = 0, 255, 0
@@ -37,12 +40,68 @@ class Tile:
         self.x = x_pos
         self.y = y_pos
         self.val = value
-    def draw(self):
-        pygame.draw.rect(screen, red, [int(self.x), int(self.y), int(self.width), int(self.height)])
-    def moveUp(self):
-        self.y = y_pos - 100
-    def isClicked(self):
-        if pygame.mouse.get_pressed()[0] and not mouseDown and pygame.mouse.get_pos()[0]>self.x and pygame.mouse.get_pos()[0]<self.x+self.width and pygame.mouse.get_pos()[1]>self.y and pygame.mouse.get_pos()[1]<self.y+self.height:
+        self.clicked = False
+    def draw(self, tiles):
+        if not self.clicked:
+          pygame.draw.rect(screen, red, [int(self.x*100+2), int(self.y*100+2), int(self.width), int(self.height)])
+        else:
+            pygame.draw.rect(screen, red, [int(pygame.mouse.get_pos()[0]-50), int(pygame.mouse.get_pos()[1]-50), int(self.width), int(self.height)])
+    def moveUp(self, tiles):
+        self.y = self.y - 1
+        if not self.y == -1:
+            return self.exchange(tiles)
+        else:
+            game_over = True
+            return tiles
+    def gravity(self, tiles):
+        print(str(self.y))
+        if not self.y == 7:
+            if tiles[self.x][self.y] is None:
+                self.y = self.y + 1
+    def isClicked(self, mouseDown):
+        if pygame.mouse.get_pressed()[0] and not mouseDown and pygame.mouse.get_pos()[0]>self.x*100 and pygame.mouse.get_pos()[0]<self.x*100+self.width and pygame.mouse.get_pos()[1]>self.y*100 and pygame.mouse.get_pos()[1]<self.y*100+self.height:
+            self.clicked = True
             mouseDown = True
-            changes = pygame.mouse.get_rel()
-        if pygame.mouse.get_pressed()[0] and mouseDown
+            return True
+        if pygame.mouse.get_pressed()[0] and mouseDown:
+            return True
+        if self.clicked and not pygame.mouse.get_pressed()[0]:
+            mouseDown = False
+            self.clicked = False
+        return False
+    def exchange(self, tiles):
+        
+        return tiles
+
+
+def dontFreeze():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            sys.exit()
+
+def play():
+    global tiles
+    while not game_over:
+        dontFreeze()
+        for x in range(len(tiles)):
+            for y in range(len(tiles[x])):
+                if not tiles[x][y] is None:
+                    if tiles[x][y].isClicked(mouseDown):
+                        print("test")
+                    elif not mouseDown and tiles[x][y].isClicked(mouseDown):
+                        print("test")
+                    else: 
+                        tiles[x][y].gravity(tiles)
+                    if time_left == 0 and not mouseDown:
+                        tiles = moveUp(tiles)
+                        if game_over:
+                            break
+                    tiles[x][y].draw(tiles)
+            if game_over:
+                break
+        pygame.display.flip()
+        pygame.draw.rect(screen, colors[background], [0, 0, screen_width, screen_height])
+
+tiles[1][1] = Tile(1, 1, 5)
+tiles[0][2] = Tile(0, 2, 5)
+play()
